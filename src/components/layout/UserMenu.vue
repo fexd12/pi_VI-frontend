d1a5ea4b4ca8cdee550b5f2bb21f10d679a46deb
fexd12 <felipe.demelo@hotmail.com>
2020-09-20 16:51:34 -0300

    alterado dropdown

--- src/components/layout/UserMenu.vue
+++ src/components/layout/UserMenu.vue
@@ -5,10 +5,13 @@
   </a> -->
 
   <div>
-    <b-dropdown v-on:click="logout" class="btn btn-default btn-flat-btn-block">
-      <b-dropdown-item>
-        <i class="fa fa-sign-out" id="exit"></i>
-        <span>Saída</span>
+    <b-dropdown class="btn btn-default btn-flat-btn-block">
+      <b-dropdown-item click="logout">
+        <!-- <i class="fa fa-sign-out" id="exit"></i>
+        <span>Saída</span> -->
+        <b-button class="fa fa-sign-out" v-on:click='logout'>
+          Saída
+        </b-button>
       </b-dropdown-item>
 
       <b-dropdown-item>
