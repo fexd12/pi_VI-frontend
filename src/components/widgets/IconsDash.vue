0e69bf06db3f937fa640107927ea43b3119e28c5
fexd12 <felipe.demelo@hotmail.com>
2020-10-25 01:43:56 -0300

    movido icons dashboard separados

--- src/components/widgets/IconsDash.vue
+++ src/components/widgets/IconsDash.vue
@@ -1,285 +1,248 @@
 <template>
   <div>
-      <div
-      class="row"
-      id="Limpeza"
-      v-if="content.funcao == 2 "
-    >
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-blue']">
-              <i :class="['ion', 'ion-ios-people-outline']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{
-                "Funcionarios de Limpeza Cadastrados"
-              }}</span>
-              <span :class="'info-box-number'">{{ usuarios.soma }}</span>
-            </div>
-          </div>
-        </template>
-        <!-- </div> -->
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-red']">
-              <i :class="['ion', 'ion-settings']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas Sujas " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-green']">
-              <i :class="['ion', 'ion-checkmark-round']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas Disponíveis " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-yellow']">
-              <i :class="['ion', 'ion-close-round']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas Alugadas " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-    </div>
-    <div
-      class="row"
-      id="Adm"
-      v-if="content.funcao == 4 "
-    >
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-blue']">
-              <i :class="['ion', 'ion-ios-people-outline']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Funcionarios Cadastrados" }}</span>
-              <span :class="'info-box-number'">{{ usuarios.soma }}</span>
-            </div>
-          </div>
-        </template>
-        <!-- </div> -->
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-black']">
-              <i :class="['ion', 'ion-thumbsdown']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas Sujas " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-green']">
-              <i :class="['ion', 'ion-checkmark-round']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas Disponíveis " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-yellow']">
-              <i :class="['ion', 'ion-close-round']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Quantidade de Salas Alugadas " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-gray']">
-              <i :class="['ion', 'ion-alert-circled']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas Interditadas " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-red']">
-              <i :class="['ion', 'ion-settings']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas em Manutenção " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-    </div>
-    <div
-      class="row"
-      id="Tecnico"
-      v-if="content.funcao == 3 "
-    >
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-gray']">
-              <i :class="['ion', 'ion-alert-circled']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas Interditadas " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-red']">
-              <i :class="['ion', 'ion-settings']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas em Manutenção " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-green']">
-              <i :class="['ion', 'ion-checkmark-round']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas Disponíveis " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-yellow']">
-              <i :class="['ion', 'ion-close-round']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas Alugadas " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-    </div>
-    <div
-      class="row"
-      id="Professor"
-      v-if="content.funcao == 1 "
-    >
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-black']">
-              <i :class="['ion', 'ion-pin']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Quantidade de Salas Alugadas " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-red']">
-              <i :class="['ion', 'ion-settings']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas em Manutenção " }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-green']">
-              <i :class="['ion', 'ion-checkmark-round']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas Disponíveis" }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-      <div class="col-lg-3">
-        <template>
-          <div class="info-box">
-            <span :class="['info-box-icon', 'bg-yellow']">
-              <i :class="['ion', 'ion-close-round']"></i>
-            </span>
-            <div class="info-box-content">
-              <span :class="'text'">{{ "Salas Alugadas" }}</span>
-              <span :class="'info-box-number'">{{ tags.soma }}</span>
-            </div>
-          </div>
-        </template>
-      </div>
-    </div>
+	<div class="row" id="Limpeza" v-if="content.id_funcao == 2">
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-blue']">
+			  <i :class="['ion', 'ion-ios-people-outline']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{
+				"Funcionarios de Limpeza Cadastrados"
+			  }}</span>
+			  <span :class="'info-box-number'">{{ limpeza.soma }}</span>
+			</div>
+		  </div>
+		</template>
+		<!-- </div> -->
+	  </div>
+
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-green']">
+			  <i :class="['ion', 'ion-checkmark-round']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{ "Salas Disponíveis " }}</span>
+			  <span :class="'info-box-number'">{{ salas_disponiveis.soma }}</span>
+			</div>
+		  </div>
+		</template>
+	  </div>
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-yellow']">
+			  <i :class="['ion', 'ion-close-round']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{ "Salas Alugadas " }}</span>
+			  <span :class="'info-box-number'">{{ salas_alugadas.soma }}</span>
+			</div>
+		  </div>
+		</template>
+	  </div>
+	</div>
+	<div class="row" id="Adm" v-if="content.id_funcao == 4">
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-blue']">
+			  <i :class="['ion', 'ion-ios-people-outline']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{ "Funcionarios Cadastrados" }}</span>
+			  <span :class="'info-box-number'">{{ teste }}</span>
+			</div>
+		  </div>
+		</template>
+		<!-- </div> -->
+	  </div>
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-green']">
+			  <i :class="['ion', 'ion-checkmark-round']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{ "Salas Disponíveis " }}</span>
+			  <span :class="'info-box-number'">{{ salas_disponiveis.soma }}</span>
+			</div>
+		  </div>
+		</template>
+	  </div>
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-yellow']">
+			  <i :class="['ion', 'ion-close-round']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{ "Quantidade de Salas Alugadas " }}</span>
+			  <span :class="'info-box-number'">{{ salas_alugadas.soma }}</span>
+			</div>
+		  </div>
+		</template>
+	  </div>
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-red']">
+			  <i :class="['ion', 'ion-settings']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{ "Salas em Manutenção" }}</span>
+			  <span :class="'info-box-number'">{{ salas_manutencao.soma }}</span>
+			</div>
+		  </div>
+		</template>
+	  </div>
+	</div>
+	<div class="row" id="Tecnico" v-if="content.id_funcao == 3">
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-red']">
+			  <i :class="['ion', 'ion-settings']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{ "Salas em Manutenção " }}</span>
+			  <span :class="'info-box-number'">{{ salas_manutencao.soma }}</span>
+			</div>
+		  </div>
+		</template>
+	  </div>
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-green']">
+			  <i :class="['ion', 'ion-checkmark-round']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{ "Salas Disponíveis " }}</span>
+			  <span :class="'info-box-number'">{{ salas_disponiveis.soma }}</span>
+			</div>
+		  </div>
+		</template>
+	  </div>
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-yellow']">
+			  <i :class="['ion', 'ion-close-round']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{ "Salas Alugadas " }}</span>
+			  <span :class="'info-box-number'">{{ salas_alugadas.soma }}</span>
+			</div>
+		  </div>
+		</template>
+	  </div>
+	</div>
+	<div class="row" id="Professor" v-if="content.id_funcao == 1">
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-black']">
+			  <i :class="['ion', 'ion-pin']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{ "Quantidade de Salas Alugadas " }}</span>
+			  <span :class="'info-box-number'">{{ salas_alugadas.soma }}</span>
+			</div>
+		  </div>
+		</template>
+	  </div>
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-red']">
+			  <i :class="['ion', 'ion-settings']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{ "Salas em Manutenção " }}</span>
+			  <span :class="'info-box-number'">{{ salas_manutencao.soma }}</span>
+			</div>
+		  </div>
+		</template>
+	  </div>
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-green']">
+			  <i :class="['ion', 'ion-checkmark-round']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{ "Salas Disponíveis" }}</span>
+			  <span :class="'info-box-number'">{{ salas_disponiveis.soma }}</span>
+			</div>
+		  </div>
+		</template>
+	  </div>
+	  <div class="col-lg-3">
+		<template>
+		  <div class="info-box">
+			<span :class="['info-box-icon', 'bg-yellow']">
+			  <i :class="['ion', 'ion-close-round']"></i>
+			</span>
+			<div class="info-box-content">
+			  <span :class="'text'">{{ "Salas Alugadas" }}</span>
+			  <span :class="'info-box-number'">{{ salas_alugadas.soma }}</span>
+			</div>
+		  </div>
+		</template>
+	  </div>
+	</div>
   </div>
 </template>
 
 <script>
+import { get_usuario } from "../../auth";
+import UsuarioVue from "../views/Usuario.vue";
+
 export default {
   name: "UsersForm",
-  props: ["value"],
   data() {
-    return {
-      content: {
-        id_usuario:this.value.id_usuario,
-        funcao: this.value.id_funcao
-      }, 
-    }
+	return {
+	  content: {},
+	  limpeza: {
+		soma: "",
+      },
+      teste:'',
+	  usuarios: {
+		soma: "",
+	  },
+	  salas_disponiveis: {
+		soma: "",
+	  },
+	  salas_alugadas:{
+		  soma:""
+	  },
+	  salas_manutencao:{
+		  soma:''
+	  }
+	};
   },
-}
+  methods: {
+	async Usuario() {
+	  this.content = { ...this.$usuario_logado };
+	},
+	async get_limpeza() {
+	  let response = await this.$http.get(
+		`${this.$baseUrl}/usuario/limpeza`,
+		{}
+	  );
+	  return response.data;
+	},
+  },
+  async mounted() {
+	await this.Usuario();
+  },
+
+};
 </script>
 
 <style>
-
 </style>
